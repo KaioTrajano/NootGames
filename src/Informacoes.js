b75739ba6ed53b9f5761ce6df33cc34252c0beb7
Kaio <kaiotrajano14@gmail.com>
2022-09-11 16:16:57 -0300

    Create Informacoes.js

---
+++ src/Informacoes.js
@@ -0,0 +1,26 @@
+function Informacoes(){
+    return (
+        <section id='greensection' className='AOSoff'>
+       <div className='greensectionimage'>
+        <img src='https://i.ibb.co/HB2bvPB/Flitterkatzen-Mount-1.png'></img>
+       </div>
+       <div className='greensectioninf'><h2>TUDO É FEITO<br></br> EM LOREM IPSUM <br></br> SEM PROBLEMA ALGUM.
+       </h2>
+       <div className='spandiv'>
+       <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+        voluptate velit esse cillum dolore </span>
+        <a href="https://www.noot.com.br/" target="_blank" rel="noreferrer">
+        <button className='buttondark'>ESTOU SOBRE LOREM IPSUM</button>
+        </a>
+       </div>
+       </div>
+
+       
+       </section>
+    )
+}
+
+export default Informacoes
