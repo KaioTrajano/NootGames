ff2ae5a22face9f02032beeb61f181cf9341472b
Kaio <kaiotrajano14@gmail.com>
2022-09-11 16:16:41 -0300

    Create Cadastro.js

---
+++ src/Cadastro.js
@@ -0,0 +1,17 @@
+function Cadastro(){
+    return (
+        <section id='cadastro'>
+        <div className='cadastroinf'>
+        <h1>LOREM IPSUM E MAIS QUE UM TEXTO SALVA VIDAS.</h1>
+         <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+          do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span><br></br>
+          <a href="https://www.noot.com.br/" target="_blank" rel="noreferrer">
+           <button className='cadastrobutton'>CADASTRAR-SE</button>
+           </a>
+        </div>
+        
+       </section>
+    )
+}
+
+export default Cadastro;
