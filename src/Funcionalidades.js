29917c339d1ee409d00ac5d6300a672a53084b01
Kaio <kaiotrajano14@gmail.com>
2022-09-11 16:16:47 -0300

    Create Funcionalidades.js

---
+++ src/Funcionalidades.js
@@ -0,0 +1,30 @@
+function Funcionalidades(){
+    return(
+        <section id='beneficios' className='AOSoff'>
+        <div className='title'>
+        <h2>PROTEÇÃO PARA ATAQUES DE LOREM IPSUM.</h2>
+        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+        </div>
+
+        <div className='itensbox'>
+         <div>
+          <img src='https://i.ibb.co/xY5Bk5t/image.png'></img>
+          <span>Controle tudo <br></br> pelo nosso <br></br> lorem <br></br> ipsum.</span>
+          </div>
+         <div><img src='https://i.ibb.co/z8zsFGz/image.png'></img>
+          <span>Configure<br></br> regras <br></br>Lorem<br></br> ipsum.</span></div>
+         <div><img src='https://i.ibb.co/j3RP0TH/image.png'></img>
+          <span>Controle tudo<br></br> pelo nosso <br></br>lorem<br></br> ipsum.</span></div>
+         <div><img src='https://i.ibb.co/WKJxtVC/image.png'></img>
+          <span>Controle tudo<br></br> pelo nosso <br></br>lorem<br></br> ipsum.</span></div>
+         <div><img src='https://i.ibb.co/XkWP8T9/image.png'></img>
+          <span>Controle tudo<br></br> pelo nosso <br></br>lorem<br></br> ipsum.</span></div>
+         <div><img src='https://i.ibb.co/DR52YhB/image.png'></img>
+          <span>Controle tudo<br></br> pelo nosso <br></br>lorem<br></br> ipsum.</span></div>
+        </div>
+        
+       </section>
+    )
+}
+
+export default Funcionalidades;
