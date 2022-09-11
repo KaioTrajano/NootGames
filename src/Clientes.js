ce642e7f01f9600f1feec4c361a24167ffee7021
Kaio <kaiotrajano14@gmail.com>
2022-09-11 16:16:44 -0300

    Create Clientes.js

---
+++ src/Clientes.js
@@ -0,0 +1,28 @@
+function Clientes(){
+    return(
+        <section id='patrocionios'>
+
+       <div className='box'>
+
+          <div className='patrociniosinf'>
+          <h2>ATÉ LOREM IPSUM, CONFIA.</h2>
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</span>
+          </div>
+          
+          <div className='marcas'>
+          <div><img src='https://i.ibb.co/WvPPfSD/outplay-white-1.png'></img></div>
+          <div><img src='https://i.ibb.co/rM0RxC7/enxadahost-1-1.png'></img></div>
+          <div><img src='https://i.ibb.co/LpFCRxq/logo-200h-1.png'></img></div>
+          <div><img src='https://i.ibb.co/jJ6SVM3/logo-1-1.png'></img></div>
+          <div><img src='https://i.ibb.co/dBS2YcF/logo-gamersclub-1.png'></img></div>
+          <div><img src='https://i.ibb.co/vzknSp5/3b25a89bce-1.png'></img></div>
+
+          
+          </div>
+       </div>
+     
+       </section>
+    )
+}
+
+export default Clientes;
