10bcbb4809c44dc7b0edc34faa1fd99ea08ed88e
Kaio <kaiotrajano14@gmail.com>
2022-09-11 16:17:11 -0300

    Create Sobre.js

---
+++ src/Sobre.js
@@ -0,0 +1,30 @@
+function Sobre(){
+    return (
+        <section id='sobre' className='moreinf'>
+        <div className='box1'>
+         <div className='moreinftitle'><h2>SOMOS UMA<br></br> EXTENSÃO EM<br></br> LOREM IPSUM.</h2>
+         <div className='spandiv'>
+         <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+          laboris nisi ut aliquip ex ea commodo consequa.</span>
+         </div>
+         </div>
+         </div>
+         <div className='box2'>
+         <div className='moreinftitle'><h2>SOMOS UMA<br></br> EXTENSÃO EM<br></br> LOREM IPSUM.</h2>
+         <div className='spandiv'>
+         <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+          laboris nisi ut aliquip ex ea commodo consequa.</span>
+         </div>
+         <a href="https://www.noot.com.br/" target="_blank" rel="noreferrer">
+        <button className='botao'>ESTOU SOBRE LOREM IPSUM</button>
+        </a>
+         </div>
+         </div>
+         
+       </section>
+    )
+}
+
+export default Sobre;
